b6ac46bfb0e7783a476127cf071ffc3bf58adb89
Apocalyptic0529 <ouenvillarandaa@gmail.com>
2026-01-10 20:06:31 +0800

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -1,13 +1,9 @@
 import express, { type Express } from "express";
 import fs from "fs";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "..", "dist", "public");
+  const distPath = path.resolve(process.cwd(), "dist", "public");
   if (!fs.existsSync(distPath)) {
     // In dev mode, we don't need this, but we'll log it
     console.warn(
